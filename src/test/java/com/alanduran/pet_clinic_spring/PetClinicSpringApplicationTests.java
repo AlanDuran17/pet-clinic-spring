5dac35b78fc8baccc35587b4cb0dea85de1d2e16
AlanDuran17 <alanduran1993@gmail.com>
2024-09-10 16:04:28 -0300

    Project initialization

---
+++ src/test/java/com/alanduran/pet_clinic_spring/PetClinicSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanduran.pet_clinic_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
