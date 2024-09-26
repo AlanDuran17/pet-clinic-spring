1838d149da57f3ed88a2cd9ecbd525d8bb2ee676
AlanDuran17 <alanduran1993@gmail.com>
2024-09-26 10:47:46 -0300

    Fix in Broken Links. Closes #45

--- pet-clinic-web/src/main/java/com/alanduran/pet_clinic_spring/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/alanduran/pet_clinic_spring/controllers/OwnerController.java
@@ -16,9 +16,15 @@ public class OwnerController {
     }
 
 
-    @RequestMapping({"", "/", ".html", "/find", "/index","/index.html"})
+    @RequestMapping({"", "/", ".html", "/index","/index.html"})
     public String listOwners(Model model) {
         model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+
+        return "notimplemented";
     }
 }
