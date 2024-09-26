e7e9570282783f14c34cf2da907b683eaf2e1e1b
AlanDuran17 <alanduran1993@gmail.com>
2024-09-26 10:15:01 -0300

    Added Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/alanduran/pet_clinic_spring/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.alanduran.pet_clinic_spring.services;
+
+import com.alanduran.pet_clinic_spring.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/alanduran/pet_clinic_spring/services/map/SpecialtyMapService.java
@@ -0,0 +1,33 @@
+package com.alanduran.pet_clinic_spring.services.map;
+
+import com.alanduran.pet_clinic_spring.model.Specialty;
+import com.alanduran.pet_clinic_spring.services.SpecialtiesService;
+
+import java.util.Set;
+
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+}
