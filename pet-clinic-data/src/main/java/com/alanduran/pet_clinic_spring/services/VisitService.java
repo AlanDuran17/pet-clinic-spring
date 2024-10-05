85ebdebea409f069c99461214b2960bb34251c2f
AlanDuran17 <alanduran1993@gmail.com>
2024-10-04 23:45:49 -0300

    Added VisitMapService. Closes #49

---
+++ pet-clinic-data/src/main/java/com/alanduran/pet_clinic_spring/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.alanduran.pet_clinic_spring.services;
+
+import com.alanduran.pet_clinic_spring.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/alanduran/pet_clinic_spring/services/map/VisitMapService.java
@@ -0,0 +1,37 @@
+package com.alanduran.pet_clinic_spring.services.map;
+
+import com.alanduran.pet_clinic_spring.model.Visit;
+import com.alanduran.pet_clinic_spring.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
