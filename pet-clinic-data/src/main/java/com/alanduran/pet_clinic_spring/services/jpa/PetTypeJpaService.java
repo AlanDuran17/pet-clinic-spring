d778a5705a6807dea9558a9f0171feb16da9e83e
AlanDuran17 <alanduran1993@gmail.com>
2024-10-04 23:15:55 -0300

    Created spring data jpa service for PetType. Closes #42

---
+++ pet-clinic-data/src/main/java/com/alanduran/pet_clinic_spring/services/jpa/PetTypeJpaService.java
@@ -0,0 +1,44 @@
+package com.alanduran.pet_clinic_spring.services.jpa;
+
+import com.alanduran.pet_clinic_spring.model.PetType;
+import com.alanduran.pet_clinic_spring.repositories.PetTypeRepository;
+import com.alanduran.pet_clinic_spring.services.PetTypeService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PetTypeJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
