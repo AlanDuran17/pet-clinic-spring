0f2b91c1681b97fc8a2e126c969b4acae2ba8cae
AlanDuran17 <alanduran1993@gmail.com>
2024-10-04 22:04:08 -0300

    Vets JPA implementation and lombok refactor . Closes #37

--- pet-clinic-data/src/main/java/com/alanduran/pet_clinic_spring/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/alanduran/pet_clinic_spring/model/Specialty.java
@@ -1,14 +1,17 @@
 package com.alanduran.pet_clinic_spring.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- pet-clinic-data/src/main/java/com/alanduran/pet_clinic_spring/model/Vet.java
+++ pet-clinic-data/src/main/java/com/alanduran/pet_clinic_spring/model/Vet.java
@@ -1,17 +1,20 @@
 package com.alanduran.pet_clinic_spring.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+               inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
-
-    public Set<Specialty> getSpecialties() {
-        return specialties;
-    }
-
-    public void setSpecialties(Set<Specialty> specialties) {
-        this.specialties = specialties;
-    }
 }
